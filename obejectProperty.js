c21158236818879ed36baaaf6c6819d0ac8b4723
poorrakib <poorrakib@gmail.com>
2024-08-25 04:18:01 +0600

    hi

---
+++ obejectProperty.js
@@ -0,0 +1,9 @@
+const students = [
+    {id: 21, name: 'rakib'},
+    {id: 38, name: 'sakib'},
+    {id: 80, name: 'bakib'},
+    {id: 92, name: 'aziz'}
+]; 
+
+const names = students.map(s => s.name);
+console.log(names);
