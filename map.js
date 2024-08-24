ddfa2b0c31303275f20dc4853a0c327a141563f1
poorrakib <poorrakib@gmail.com>
2024-08-25 04:10:46 +0600

    fd

--- map.js
+++ map.js
@@ -2,3 +2,9 @@ const number = [3, 4, 5, 6, 7, 8];
 
 const result = number.map(x => x * x );
 console.log(result);
+
+const bigger = number.filter(x => x > 5);
+console.log(bigger);
+
+const isThere = number.find( x => x > 5);
+console.log(isThere);
