62e3d126df342aebd14ad4e663ee334bc251ed16
poorrakib <poorrakib@gmail.com>
2024-08-25 04:06:22 +0600

    hi

--- double.js
+++ double.js
@@ -7,3 +7,15 @@ if(first == second){
 else{
     console.log("condition is false");
 }
+
+/// vs ===
+
+const first1 = 2;
+const second1 = "2";
+
+if(first1 === second1){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
---
+++ map.js
@@ -0,0 +1,4 @@
+const number = [3, 4, 5, 6, 7, 8];
+
+const result = number.map(x => x * x );
+console.log(result);
